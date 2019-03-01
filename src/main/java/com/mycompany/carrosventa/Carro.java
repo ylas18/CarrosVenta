935074f695d82428f55dfe82eab0a7b9ed486dae
Yesid Avila <Yesid Avila@YLAS-ASUS>
2019-03-01 17:53:48 -0500

    Añado atributos a la clase padre carro Creo clase Inventario Creo clase Principal

--- src/main/java/com/mycompany/carrosventa/Carro.java
+++ src/main/java/com/mycompany/carrosventa/Carro.java
@@ -11,4 +11,36 @@ package com.mycompany.carrosventa;
  */
 public class Carro {
     
+    /**
+     * Variable que identifica cada carro de manera unica
+     */
+    private int codigo;
+    /**
+     *  Variable que almacena la marca de cada carro
+     */
+    private String marca;
+    /**
+     * Variabe que alamcena el color de cada carro
+     */
+    private String color;
+    /**
+     * Variable que almacena el modelo de cada carro
+     */
+    private int modelo;
+    /**
+     * Variable que almacena la capacidad de pasajeros que tiene cada carro
+     */
+    private int capacidadPasajeros;
+    /**
+     * Variable que almacena el precio de cada carro
+     */
+    private double precio;
+    /**
+     * Variable que amacena dl tipo de combustible que consume cada carro
+     */
+    private String combustuble;
+    /**
+     * Variable que almacena el cilindraje de cada motor que tiene el carro
+     */
+    private int cilindraje;
 }
---
+++ src/main/java/com/mycompany/carrosventa/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.carrosventa;
+
+import javax.swing.JList;
+
+/**
+ *
+ * @author Yesid Avila
+ */
+public class Principal {
+    public Principal(){
+    
+    JList listaNombres;
+    String nombres[] = { "Cristian", "Julian", "Milena"};
+    listaNombres = new JList( nombres );
+
+    
+    }
+    
+}
