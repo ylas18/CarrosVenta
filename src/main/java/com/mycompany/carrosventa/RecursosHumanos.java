2e13a0ea90658e5cd02bef96b18cb5b107e89074
Yesid Avila <Yesid Avila@YLAS-ASUS>
2019-03-02 20:26:33 -0500

    Modifico los metodos de inicio

---
+++ src/main/java/com/mycompany/carrosventa/RecursosHumanos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.carrosventa;
+
+/**
+ *
+ * @author Yesid Avila
+ * @version 1.0
+ * Clase que maneja las personas que estan en el concesionario
+ */
+public class RecursosHumanos {
+    
+    public void mostrarClientes(){
+    
+        
+    
+    
+    }
+    
+}
